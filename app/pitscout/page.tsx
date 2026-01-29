7a278ec0e3f96cafe412db398841d17aa528a253
ihouston2137 <ian.houston.2137@gmail.com>
2026-01-28 20:41:50 -0500

    Update Pit Report Example

---
+++ app/pitscout/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+
+
+const Page = () => {
+
+
+return (<>
+    <h1>Pit Scout</h1>  
+    <h2>Scouting Reports</h2>
+
+    <h3>Scouting Reports</h3>
+    <h4>Scouting Reports</h4>
+
+</>)
+
+
+}
+
+export default Page
