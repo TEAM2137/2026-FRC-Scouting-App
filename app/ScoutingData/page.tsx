595b07e1526b0e0342b688fd5cb84bc095d9cb12
Caleb H - Student <masterfrederick7410@gmail.com>
2026-01-31 15:47:19 -0500

    fixed the header and footer
    fixed the header and footer but the closer is causing problems with it and is spitting an error

--- app/ScoutingData/page.tsx
+++ app/ScoutingData/page.tsx
@@ -2,14 +2,16 @@
 
 const ScoutingData1 = () => {
  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Welcome to 2026-scouting-app</h1>
-        <h2>This-Is-The-Post-match-report-Page Copywright 2026 2137 TORC</h2>
-      </main>
-     <header className ="header-class">Header Content</header>
-    </div>
-    
+    <><div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+       <h1>Welcome to 2026-scouting-app</h1>
+       <h2>This-Is-The-Post-match-report-Page Copywright 2026 2137 TORC</h2>
+     </main>
+     <header className="header-class">Header Content</header>
+   <div><footer className="fixed bottom-0 w-full bg-blue-600 text-center p-2 text-slate-100">Copywright 2026 2137 TORC</footer></>
+   <div><header className="fixed top-0 w-full bg-green-600 text-center p-2 text-slate-100">This is the Header</header></div>
+  </>
   );
+
 }
 export default ScoutingData1;
