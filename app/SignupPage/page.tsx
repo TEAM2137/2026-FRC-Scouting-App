fc164bf361a37f69f725e27c4a53c8d493369715
Caleb H - Student <masterfrederick7410@gmail.com>
2026-02-01 11:27:34 -0500

    Added the signup prompt
    I have added the signup prompt from shadcn/ui and is still trying to find and figure out to get full functionality.

--- app/SignupPage/page.tsx
+++ app/SignupPage/page.tsx
@@ -7,9 +7,69 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const thisIsTheSignupPage = () => {
   return (
+
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+export function CardDemo() {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+        <CardAction>
+          <Button variant="link">Sign Up</Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+
+
+
    <><div className="flex min-h-screen items-center justify-center bg-blue-500">
         <h1> Welcome to the Signup Page of 2026-scouting-app </h1>
         <Card>
