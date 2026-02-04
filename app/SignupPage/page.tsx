914540d97ac572000867d18b4ce708edaa62bf8b
ihouston2137 <ian.houston.2137@gmail.com>
2026-02-03 20:52:18 -0500

    IanH Examples in meeting

--- app/SignupPage/page.tsx
+++ app/SignupPage/page.tsx
@@ -10,54 +10,15 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
-const thisIsTheSignupPage = () => {
+
+import SignInForm from "@/components/auth/SignInForm"
+
+const Page = () => {
   return (
-      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-        </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-1">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="RoboticsTeam@roboticsTeam.com"
-                required
-              />
-            </div>
-            <div className="center grid gap-3">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id="password" type="password" required placeholder= "P455w0rd H3r3" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-      </CardFooter>
-    </Card>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <SignInForm />
    </div>
   );
 }
 //get the user input for email and password finished on tuesday
-export default thisIsTheSignupPage;
+export default Page;
---
+++ models/auth/TeamAdmin.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+interface TeamManager {
+
+
+}
