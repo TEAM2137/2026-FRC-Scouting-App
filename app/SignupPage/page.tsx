a368e33be7e6b67c96d2ff47204b115bd7d44bb7
Caleb H - Student <masterfrederick7410@gmail.com>
2026-02-04 19:07:31 -0500

    fixed and finnished the ui

--- app/SignupPage/page.tsx
+++ app/SignupPage/page.tsx
@@ -38,9 +38,17 @@ const Page = () => {
         </CardHeader>
         <CardContent>
           <form>
-            <div className="flex flex-col gap-6">
+             <div className="flex flex-col gap-3">
+               <div className="center grid gap-3">
+               <Label htmlFor="TeamNumber">Team Number</Label>
+                <Input id="TeamNumber" type="text" required placeholder="12345"/>
+           </div>
+            <div className="center grid gap-3">
+               <Label htmlFor="name"> Manager Name</Label>
+                <Input id="name" type="text" required placeholder="John Smith"/>
+              </div>
               <div className="grid gap-1">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">Manager Email</Label>
                 <Input
                   id="email"
                   type="email"
@@ -48,26 +56,33 @@ const Page = () => {
                   required
                 />
               </div>
-              <div className="center grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
+              <div className="grid gap-3">
+               <Label htmlFor="password">Manager Password</Label>
+                <Input id="password" type="password" required/>
               </div>
               <div>
-                <Input id="password" type="password" required placeholder="P455w0rd H3r3" />
-                <Button type="submit" className="w-full">
-                  Sign In
-                </Button>
-              </div>
-              <div>
-                <label htmlFor="PhoneNumber">Phone Number</label>
+                <label htmlFor="PhoneNumber">Manager Phone Number</label>
                 <Input id="PhoneNumber" type="tel" placeholder="123-456-7890" required />
               </div>
+              <div className="grid gap-3">
+                <label htmlFor="role">Role on team</label>
+                <select id="role" required>
+                  <option value="">Select your role</option>
+                  <option value="manager">FIRST Lead Mentor</option>
+                  <option value="coach">Adult Mentor on Team</option>
+                  <option value="scout">Student Leader on Team</option>
+                </select>
+
+
+              </div>
+              
               <div>
                 <Checkbox className="h-6 w-6 text-green-500" required />
                 <label htmlFor="terms">I am responsible for my and my teams actions. By using this app, your team will not add bad data into the database and will input data that is meaningful to the database</label>
               </div>
-              
+              <div><Button type="submit" className="w-full">
+                  Sign Up 
+                </Button></div>
             </div>
           </form>
         </CardContent>
