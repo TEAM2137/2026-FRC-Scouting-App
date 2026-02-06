56b6df2d5832e1027c62c9e134b6f9c02826313e
Evan-209 <evan.ruhlman@gmail.com>
2026-02-05 19:04:46 -0500

    yy

--- app/Goobly.tsx
+++ app/Goobly.tsx
@@ -1,7 +1,9 @@
 export default function rome() {
     <html>
         <body>
-            
+            <button>
+                wasn't built in a day
+            </button>
         </body>
     </html>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Welcome to 2026-scouting-app</h1>
-        <img src="Shaw by Shaw.gif"></img>
+        <button>
+          rome wasn't...
+        </button>
         <Image src="/webapp-icons/android-chrome-512x512.png" alt="Logo" width="200" height="200" />
       </main>
     </div>
