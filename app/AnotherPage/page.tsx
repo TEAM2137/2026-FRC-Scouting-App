1b22c3660e02c3677c26d584924c5ca09b81bf09
Evan-209 <evan.ruhlman@gmail.com>
2026-02-07 15:29:42 -0500

    Update page.tsx

--- app/AnotherPage/page.tsx
+++ app/AnotherPage/page.tsx
@@ -9,21 +9,18 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-import React, { useState } from 'react';
+import React, { cloneElement, useState } from 'react';
 import Image from "next/image";
 
 
 
 export default function Home() {
-    const [score, setScore] = useState(0);
-    const getInfo = () => {
-        
-    }
     return (
-        <html>
-            <body>
-                <p className="bg-purple-400"> 1234 match</p>
-            </body>
+           <html>
+
+          <body>
+            theres nothing
+          </body>
         </html>
     );
 }
