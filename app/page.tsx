dd1d72871f02d47fd05228095e031673e1c9d6f9
Evan-209 <evan.ruhlman@gmail.com>
2026-02-07 08:39:49 -0500

    good code

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Card,
   CardAction,
@@ -8,17 +9,55 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+import React, { useState } from 'react';
+
 export default function Home() {
+
+    const [fuel, setFuel] = useState(0)
+    const [DeInAmount, setDeInAmount] = useState(1)
+
+    const increaseFuel = () => {
+        setFuel(fuel + DeInAmount);
+    }
+    const decreaseFuel = () => {
+        if (fuel > 0 && fuel - DeInAmount > 0) {
+            setFuel(fuel - DeInAmount);
+        } else {
+            setFuel(0)
+        }
+    }
+    const ChangeFuel = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (Number(event.target.value)%1 == 0) {
+            setDeInAmount(Number(event.target.value));
+        }
+        if (Number(event.target.value) > 50) {
+            setDeInAmount(50)
+        }
+    }
+
+    return (
     <html>
         <body>
-            <Card style={{ border: "2px solid #3b82f6" }}>
-                <CardHeader id = "yodelelo">Team Number: </CardHeader>
+            <Card className="bg-blue-800">
+                <CardHeader className="bg-yellow-500 rounded-2xl" id ="yodelelo">Team Number: 
+                    <select id="yellow">
+                        <option value="1">2137</option>
+                        <option value="2">2138</option>
+                        <option value="3">70073</option>
+                    </select>
+                </CardHeader>
                 <CardContent>
-                    <button id="yodel">Get Team Number</button>
-                    
+                    <button onClick={increaseFuel} className="bg-green-600 rounded-4xl"id="Increase1">+{DeInAmount} fuel</button>
+
+                    <center><p className="font-bold text-2xl"id = "fuelAmount"> {fuel} </p></center>
+                    <button onClick={decreaseFuel} className="bg-red-600 rounded-4xl" id="Decrease1">-{DeInAmount} fuel</button>
                 </CardContent>
+                <CardDescription>
+                    <center><p className="text-white">Fuel Change Amount</p><input className="bg-blue-950 rounded-4xl text-white text-center"onChange={ChangeFuel} value={DeInAmount}></input></center>
+                </CardDescription>
             </Card>
-            <script src="mrbeast.js"></script>
+            
         </body>
     </html>
+    );
 }
