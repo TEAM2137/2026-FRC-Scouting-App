5c5d3591a1b6c075e21fba8bea5c7f7bae8e5255
Andrew_P <andrewmprevo@gmail.com>
2026-02-11 18:42:49 -0500

    stuff

--- app/api/examples/frc-events/teams/route.ts
+++ app/api/examples/frc-events/teams/route.ts
@@ -4,13 +4,15 @@ import connectDB from "@/lib/db";
 import Team from "@/models/FRC-API/Team";
 
 export async function GET(request: NextRequest) {
-    const apiURL = "https://frc-api.firstinspires.org/v3.0";
+    const apiURL = "https://frc-api.firstinspires.org/v3.0/2026/teams";
     const headers = {
         'authorization': `Basic ${process.env.FRC_API_KEY}`,
         'content-Type': 'application/json'
     }
     const response = await fetch(apiURL,{headers: headers });
     const data = await response.json();
+
+
     await connectDB();
 
     for (let i = 0; i < data.teams.length; i++) {
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ return (
     <div className="col-span-1 text-left">
     <h1> Our scouting app logo</h1>
 </div> 
-<div className="col-span-1 text-right rounded-lg bg-black p-2">
+<div className="col-span-1 text-right rounded-lg bg-black p-1.5" style={{width: "100px", height:"65px", position: "absolute", right: "0px", top: "0px"}}>
 <h1>Our scouting app menu</h1>
 </div> 
 </div>
