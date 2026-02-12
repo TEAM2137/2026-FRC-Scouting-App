1463cb81dcb95e974881387a7538abc8ef992075
Evan-209 <evan.ruhlman@gmail.com>
2026-02-11 20:19:06 -0500

    yws

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import { SignalZero } from 'lucide-react';
 import Image from 'next/image';
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+  <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Welcome to 2026-scouting-app</h1>
-        <Image src="/webapp-icons/android-chrome-512x512.png" alt="Logo" width="200" height="200" />
+        <h1>balls</h1>
+  
       </main>
     </div>
   );
