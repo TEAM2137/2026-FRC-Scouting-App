ea5393c16212e417b52aeae86e4a185ac6211160
Evan-209 <evan.ruhlman@gmail.com>
2026-02-14 14:05:50 -0500

    What is this blud doing???

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,14 @@ import {
 } from "@/components/ui/card"
 
 export default function Home() {
+  
+  const clickyBlicky = () => {
 
+  }
   return (
     <div className="grid grid-col h-screen w-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="grid grid-col h-screen w-full items-center justify-center place-items-center py-2 px-2 bg-white dark:bg-black sm:items-start">
+        <button onClick={}> yodeleleleleleo </button>
         <SignIn/>
       </main>
     </div>
