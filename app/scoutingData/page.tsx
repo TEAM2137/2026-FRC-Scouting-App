2dd52392793a1371a3abdcbc1c4301dcb0df227d
Andrew_P <andrewmprevo@gmail.com>
2026-02-14 16:19:46 -0500

    doesn't work

--- app/scoutingData/page.tsx
+++ app/scoutingData/page.tsx
@@ -2,18 +2,18 @@
 
 
 export default function Page() {
-    return (<div className="mt-100">
-        <div className='bg-blue-500 text-white p-4 rounded-lg fixed center-0 left-0'style={{top: '48px', left: '44.5%'}}>scouting data page</div>
+    return (<div className="pt-100 w-screen">
+        <div className='bg-blue-500 text-white p-4 rounded-lg '>Scouting data page</div>
 
         
-    
-    <h1 className='bg-blue-500  text-white p-4 rounded-lg fixed center-0 left-0'style={{width: '48%', height: '200px', top: '106px', left: '1.5%', fontSize: '25px'}}>Match 1</h1>
-
-    <h1 className='bg-blue-500 text-white p-4 rounded-lg fixed center-0 left-0' style={{width: '48%', height: '200px', top: '106px', left: '50%', fontSize: '25px'}}>Match 2</h1>
- <h1 className='bg-blue-500  text-white p-4 rounded-lg fixed center-0 left-0'style={{width: '48%', height: '200px', top: '310px', left: '1.5%', fontSize: '25px'}}>Match 3</h1>
-     <h1 className='bg-blue-500 text-white p-4 rounded-lg fixed center-0 left-0' style={{width: '48%', height: '200px', top: '310px', left: '50%', fontSize: '25px'}}>Match 4</h1>
-   <h1 className='bg-blue-500  text-white p-4 rounded-lg fixed center-0 left-0'style={{width: '48%', height: '200px', top: '514px', left: '1.5%', fontSize: '25px'}}>Match 5</h1>
-   <h1 className='bg-blue-500 text-white p-4 rounded-lg fixed center-0 left-0' style={{width: '48%', height: '200px', top: '514px', left: '50%', fontSize: '25px'}}>Match 6</h1>
+    <div className="grid grid-cols-2 gap-4 mt-4">
+     <div className='flex bg-blue-500 text-white p-4 rounded-lg w-100 h-5  '><h1 className=' '>Match 1</h1> </div>
+    <div className='flex bg-blue-500 text-white p-4 rounded-lg w-100 h-5  '><h1 className=' '>Match 2</h1> </div>
+    <div className='flex bg-blue-500 text-white p-4 rounded-lg w-100 h-5  '><h1 className=' '>Match 3</h1> </div>
+    <div className='flex bg-blue-500 text-white p-4 rounded-lg w-100 h-5  '><h1 className=' '>Match 4</h1> </div>
+    <div className='flex bg-blue-500 text-white p-4 rounded-lg w-100 h-5  '><h1 className=' '>Match 5</h1> </div>
+    <div className='flex bg-blue-500 text-white p-4 rounded-lg w-100 h-5  '><h1 className=' '>Match 6</h1> </div>
+        </div>
    
    </div> );
    }
