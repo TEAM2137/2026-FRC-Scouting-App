55be621a23af35e80037dd7839c6dc54c0fa62f9
Caleb H - Student <masterfrederick7410@gmail.com>
2026-02-14 16:20:10 -0500

    working on the studnt signup

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SignIn from '@/components/auth/SignIn';
 import SignUpTeam from '@/components/auth/SignUpTeam';
 import Image from 'next/image';
 import { useState } from 'react';
-
+import SignUpScout from '@components/auth/SignUpScout'
 export default function Home() {
   const [authDisplay, setAuthDisplay] = useState('signin');
 
@@ -32,7 +32,13 @@ export default function Home() {
         </>
         }
 
-
-    </div>
+{authDisplay === "signupstudent" &&
+<>
+<SignUpScout/>
+<button onClick={() => setAuthDisplay("signup")}>
+If you are a student then signup here</button>
+</>
+}
+</div>
   );
 }
