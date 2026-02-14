aa7081a7b5e788bc05fa86160ce95522bd2a731b
Caleb H - Student <masterfrederick7410@gmail.com>
2026-02-14 15:20:24 -0500

    changed the forgot password
    theres a form in the forgot password screen now

--- components/auth/ForgotPassword.tsx
+++ components/auth/ForgotPassword.tsx
@@ -1,15 +1,42 @@
-'use client'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
-
-const ForgotPassword = () => {
-    return (
-        <div className="flex flex-col items-center justify-center gap-4 p-4">
-            <h1 className="text-3xl font-bold">Forgot Password</h1>
-            <p className="text-lg">
-                Forgot your password? No problem! We'll send you a reset link.
-            </p>
-        </div>
-    )
+const Page = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-comicsans dark:bg-black">
+      {/* You must wrap the sub-components in the main Card component */}
+      <Card className="w-[350px]"> 
+        <CardHeader>
+          <CardTitle>Forgot Password</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-3">
+            <Label htmlFor="email">Manager Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="John.Doe@email.com"
+              required
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          {/* w-full makes the button span the full width of the card */}
+          <Button variant="outline" className="w-full" type="submit">
+            Send Reset Link
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
 }
 
-export default ForgotPassword
+export default Page
