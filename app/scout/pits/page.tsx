e1324fead15e918fff5ecbbdb5921c9a75bce502
Evan-209 <evan.ruhlman@gmail.com>
2026-02-19 19:56:02 -0500

    Nothing Burger changes (All I did was add a card and a not finished pit scouting page)
    yeah i summed it up in the title

--- app/scout/pits/page.tsx
+++ app/scout/pits/page.tsx
@@ -6,7 +6,15 @@ import { useRouter } from 'next/navigation';
 import { useAppContext } from '@/context/AppContext';
 import { getToken } from '@/lib/jwt';
 import { set } from 'mongoose';
-
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 
 
@@ -20,6 +28,11 @@ return (
     <div className="flex flex-col w-screen pl-20 text-xs">
         <h1 className="text-xl font-bold">{event?.name}</h1>
         <p>Pit scout robots at the event.</p>
+        <Card style={{height:"300px",width:"600px"}}>
+            <CardHeader className="text-center" style={{ backgroundColor:"#fecd07"}}>boba tea</CardHeader>
+             
+            <CardFooter className="text-center" style={{ backgroundColor:"#fecd07"}}>boba tea</CardFooter>
+        </Card>
     </div>
 )
 }
