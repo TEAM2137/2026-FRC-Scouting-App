1879d3745523686b85d317959f93b282b2cb8199
Caleb H - Student <masterfrederick7410@gmail.com>
2026-02-21 09:55:35 -0500

    added some pit scouting questions and imputs

--- app/scout/pits/page.tsx
+++ app/scout/pits/page.tsx
@@ -1,6 +1,14 @@
 'use client'
+import { Input } from '@/components/ui/input';
+import { Label } from 'radix-ui';
+import SideBar from '@/components/app/SideBar';
+import { Checkbox } from '@/components/ui/checkbox';
+import { CardHeader } from '@/components/ui/card';
+import { CardTitle } from '@/components/ui/card';
+import { CardDescription } from '@/components/ui/card';
 
-
+import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAppContext } from '@/context/AppContext';
@@ -17,11 +25,81 @@ const Page = () => {
 
     
 return (
-    <div className="flex flex-col w-screen pl-20 text-xs">
+    <><div className="flex flex-col w-screen pl-20 text-xs">
         <h1 className="text-xl font-bold">{event?.name}</h1>
         <p>Pit scout robots at the event.</p>
+   </div>
+   
+   
+   
+   <Card>
+    <CardHeader>
+<CardTitle>
+    <CardDescription>
+<div classname="flex flex-col gap-3">
+<Label htmlFor="number">How much fuel can this team carry</Label>
+<Input
+id="number"
+type="text"
+required
+placeholder="12345"
+value={FormData.number}
+onChange={(e) => {setFormData({...FormData, number: e.target.value})}></Input>
+    
+    <div className='grid gap-3'>
+<Label htmlFor="number">What is their average fuel score</Label>
+<Input
+id="number"
+type="text"
+placeholder="12345"
+required>
+</Input>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    </CardDescription>
+</CardTitle>
+    </CardHeader>
+   </Card>
+   
+   
+   
+    
+
+
+
+
+
+
+
+
+
+</>
 )
 }
 
-export default Page;
+export default Page
