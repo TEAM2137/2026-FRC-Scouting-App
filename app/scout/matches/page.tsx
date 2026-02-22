7d75c259832b4a8e348a7e1f7b589e7395c73f4f
Caleb H - Student <masterfrederick7410@gmail.com>
2026-02-22 09:46:48 -0500

    added the match scout schema to the matches page.

--- app/scout/matches/page.tsx
+++ app/scout/matches/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { useAppContext } from '@/context/AppContext';
 import { getToken } from '@/lib/jwt';
 import { set } from 'mongoose';
+import { IMatchscout } from '@/models/frc-events/matchScout/matchScout';
 
 
 
@@ -13,9 +14,24 @@ import { set } from 'mongoose';
 const Page = () => {
     const router = useRouter();
     const { event, setAppEvent } = useAppContext();
-    
+    const [formData, SetFormData] = useState<IMatchscout>({
+      totalShotsAuto: 0,
+      totalPassedAuto: 0,
+      totalHerded: 0,
+      totalPassedTeleop: 0,
+      totalShotsTeleop: 0,
+      climbLevelTeleop: 0,
+      didDie: 0,
+      didBreak: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      totalScored: 0,
+      didAutoClimb: 0,
 
-    
+
+    });
+
+
 return (
     <div className="flex flex-col w-screen pl-20 text-xs">
         <h1 className="text-xl font-bold">{event?.name}</h1>
