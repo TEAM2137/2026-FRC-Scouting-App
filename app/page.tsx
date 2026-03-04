748c5b051861681a0d7394bda0542542ed21ddd0
Caleb H - Student <masterfrederick7410@gmail.com>
2026-03-04 17:08:47 -0500

    pulling things from main to have the ui working.

--- app/page.tsx
+++ app/page.tsx
@@ -6,33 +6,19 @@ import Image from 'next/image';
 import { useState } from 'react';
 import SignUpScout from '@/components/auth/SignUpScout'
 export default function Home() {
-  
-   const [authDisplay, setAuthDisplay] = useState('signin');
+  const [authDisplay, setAuthDisplay] = useState('signin');
+
   return (
     <div className="grid grid-col h-screen w-screen pl-16 justify-center place-items-center  bg-zinc-50 font-sans dark:bg-neutral-900 ">
         
         <div className="flex flex-col w-full p-2 gap-2 place-items-center">
           <Image src="/images/TEAM2137-Robocat-Head.svg" alt="Acme Logo" width={100} height={50} />
-          <div className="text-lg font-bold">NEXT SCOUT</div>
           <div className="text-sm font-normal italic">Welcome to our scouting app for 2026!</div>
+          <Image src="/images/ABACUS-white-4x5.svg" alt="Abacus Logo" width={200} height={250} />
+          <div className="text-2xl font-bold">ABACUS</div>
         </div>
 
-        {authDisplay === 'signin' && 
-        <>
-        <SignIn />
-        <button onClick={() => setAuthDisplay('signupteam')}>
-        If your team is not registered, you can sign up for a team here.</button>
-        <button onClick={() => setAuthDisplay('forgotpassword')}>
-        If you forgot your password you can reset it here.</button>
-        </>
-        }
-        {authDisplay === 'signupteam' && 
-        <>
-        <SignUpTeam />
-        <button onClick={() => setAuthDisplay('signin')}>
-        If you already have a team, you can sign in here.</button>
-        </>
-        }
+        
 
 
     </div>
