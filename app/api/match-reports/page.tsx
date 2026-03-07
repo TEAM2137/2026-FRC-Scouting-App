ddc16a2e05bed362bc5c749e96a25576da8e331e
Caleb H - Student <masterfrederick7410@gmail.com>
2026-03-07 13:11:53 -0500

    testing a match report page

---
+++ app/api/match-reports/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import {Card, CardDescription} from "@/components/ui/card"
+import { IMatchscout,  Matchscoutschema } from "@/models/frc-events/matchScout/matchScout"
+    
+    const page = () => {
+
+        const sortingValues{
+            if
+        }
+return(
+<div className=" h-screen w-screen z-10">
+    <Card>
+<CardDescription>the match results of each team
+</CardDescription>
+
+    </Card>
+</div>
+
+)
+    }
+    export default page
