47de68422da9a2d64a3a6c2b795977ab69b05d5e
Caleb H - Student <masterfrederick7410@gmail.com>
2026-03-08 09:58:05 -0400

    currently working on making a match results screen and analytics

--- app/api/match-reports/page.tsx
+++ app/api/match-reports/page.tsx
@@ -1,19 +1,20 @@
 'use client'
 
 import {Card, CardDescription} from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
 import { IMatchscout,  Matchscoutschema } from "@/models/frc-events/matchScout/matchScout"
     
     const page = () => {
 
-        const sortingValues{
-            if
-        }
+       
 return(
-<div className=" h-screen w-screen z-10">
+<div className="grid gap-3">
     <Card>
-<CardDescription>the match results of each team
-</CardDescription>
+<CardDescription>the match results of each team</CardDescription>
+   <div className="text-center">
+    <Label>match results</Label>
 
+   </div>
     </Card>
 </div>
 
