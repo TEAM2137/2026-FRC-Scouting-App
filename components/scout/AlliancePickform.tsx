80719cd407b3536c4db48f108b85fd587d3518f8
Caleb H - Student <masterfrederick7410@gmail.com>
2026-03-25 20:19:28 -0400

    added the picklist ofrm team rankings
    still working out things not finished

--- components/scout/AlliancePickform.tsx
+++ components/scout/AlliancePickform.tsx
@@ -4,6 +4,10 @@ import { Label } from "../ui/label"
 import { Button } from "../ui/button"
 import { Checkbox } from "../ui/checkbox"
 import { useState } from "react"
+import { useEffect } from "react"
+import { getRankings } from "@/lib/bluealliance/getRankings"
+import { useAppContext } from "@/context/AppContext"
+
 const  AlliancePickform = () => {
    const sendtoDB = () => {
     //send to db
@@ -13,7 +17,65 @@ const  AlliancePickform = () => {
     eventCode: "",
     picks: []
    })
-   
+  
+}
+ interface IProps {
+
+    eventCode: string,
+   }
+const AllianceTools = ({eventCode}: IProps) => {
+    const [alliances, setAlliances] = useState<string[]>([]);
+    const [rankings, setRankings] = useState<any[]>([]);
+    const [displayRankings, setDisplayRankings] = useState<any[]>([]);
+    const [showEventRankings, setShowEventRankings] = useState<boolean>(true);
+    const [showPickLists, setShowPickLists] = useState<boolean[]>([true,true,true,true,true]);
+    const { appEvent } = useAppContext();
+
+    const getTeamName = (teamNumber: string) => {
+        if (appEvent?.teams) {
+            const team = appEvent?.teams.find((team) => team.number === teamNumber);
+            if (team) {
+                return team.name;
+            }
+        }
+        return teamNumber;
+    }
+
+    const addSelection = (teamNumber: string) => {
+        //const updatedAliances = alliances
+        //updatedAliances.push(teamNumber);
+        setAlliances(alliances.concat(teamNumber));
+    }
+
+    const subtractAlliance = () => {
+        const updatedAlliances = alliances.filter((alliance) => alliance !== alliances[alliances.length - 1]);
+        setAlliances(updatedAlliances);
+    }
+
+    const handleShowPickLists = (index: number) => {
+        const updatedShowPickLists = showPickLists.map((showPickList, i) => i === index ? !showPickList : showPickList);
+        setShowPickLists(updatedShowPickLists);
+    }
+
+    useEffect(() => {
+        const fetchRankings = async () => {
+            const rankings = await getRankings(eventCode);
+            if (rankings !== null) {
+                setRankings(JSON.parse(rankings));
+            }
+        }
+        fetchRankings();
+    }, []);
+
+    useEffect(() => {
+        if (rankings.length > 0) {
+            setDisplayRankings(rankings);
+        }
+    }, [rankings]);
+
+    useEffect(() => {
+        setDisplayRankings(rankings.filter((ranking) => !alliances.includes(ranking.teamNumber)));
+    }, [alliances]);
  return (
     <Card>
         <CardContent>
