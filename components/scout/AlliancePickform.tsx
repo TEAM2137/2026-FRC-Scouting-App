2751bbda849ea984db8a17e0a25adbed3852fddd
Caleb H - Student <masterfrederick7410@gmail.com>
2026-03-25 20:25:06 -0400

    commit

--- components/scout/AlliancePickform.tsx
+++ components/scout/AlliancePickform.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button"
 import { Checkbox } from "../ui/checkbox"
 import { useState } from "react"
 import { useEffect } from "react"
-import { getRankings } from "@/lib/bluealliance/getRankings"
+import { getRankings } from "@/lib/scout/getRankings"
 import { useAppContext } from "@/context/AppContext"
 
 const  AlliancePickform = () => {
