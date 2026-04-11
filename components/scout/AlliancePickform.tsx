dc8256de5ffb7217ebca845c602fd2ca1676141b
Caleb H - Student <masterfrederick7410@gmail.com>
2026-04-11 12:51:43 -0400

    changing the form on this branch to edit

--- components/scout/AlliancePickform.tsx
+++ components/scout/AlliancePickform.tsx
@@ -1,35 +1,29 @@
-import { Card, CardContent } from "../ui/card"
-import { Input } from "../ui/input"
-import { Label } from "../ui/label"
-import { Button } from "../ui/button"
-import { Checkbox } from "../ui/checkbox"
-import { useState } from "react"
-import { useEffect } from "react"
-import { getRankings } from "@/lib/scout/getrankings"
+'use client'
+
+import { useState, useEffect } from "react"
 import { useAppContext } from "@/context/AppContext"
 
-const  AlliancePickform = () => {
-   const sendtoDB = () => {
-    //send to db
-   }
-   const [data, setData] = useState({
-    PickListName: "",
-    eventCode: "",
-    picks: []
-   })
-  
-}
- interface IProps {
+import { getRankings } from '@/lib/scout/getrankings';
+import { get } from "http";
+import { CircleArrowRight, ArrowRight, CircleArrowUp, CircleArrowDown, CircleMinus } from "lucide-react";
 
+
+ interface IProps {
     eventCode: string,
+    list: string,
+    closeForm: () => void
    }
-const AllianceTools = ({eventCode}: IProps) => {
-    const [alliances, setAlliances] = useState<string[]>([]);
+
+
+const AlliancePickForm = ({eventCode, list}: IProps) => {
+    const [picks, setPicks] = useState<string[]>([]);
+    const [newPicks, setNewPicks] = useState<string[]>([]);
     const [rankings, setRankings] = useState<any[]>([]);
     const [displayRankings, setDisplayRankings] = useState<any[]>([]);
-    const [showEventRankings, setShowEventRankings] = useState<boolean>(true);
+    
     const [showPickLists, setShowPickLists] = useState<boolean[]>([true,true,true,true,true]);
     const { appEvent } = useAppContext();
+    const [listName, setListName] = useState(list);
 
     const getTeamName = (teamNumber: string) => {
         if (appEvent?.teams) {
@@ -42,20 +36,27 @@ const AllianceTools = ({eventCode}: IProps) => {
     }
 
     const addSelection = (teamNumber: string) => {
-        //const updatedAliances = alliances
-        //updatedAliances.push(teamNumber);
-        setAlliances(alliances.concat(teamNumber));
+        setNewPicks(newPicks.concat(teamNumber));
     }
 
-    const subtractAlliance = () => {
-        const updatedAlliances = alliances.filter((alliance) => alliance !== alliances[alliances.length - 1]);
-        setAlliances(updatedAlliances);
+    const subtractSelection = (teamNumber: string) => {
+        const updatedPicks = picks.filter((pick) => pick !== teamNumber);
+        setNewPicks(updatedPicks);
     }
 
-    const handleShowPickLists = (index: number) => {
-        const updatedShowPickLists = showPickLists.map((showPickList, i) => i === index ? !showPickList : showPickList);
-        setShowPickLists(updatedShowPickLists);
+    const moveSelection = (index: number, direction: string) => {
+        const updatedPicks = picks
+        let targetIndex = direction === "up" ? index - 1 : index + 1;
+
+        if (targetIndex < 0 || targetIndex >= updatedPicks.length) {
+            return;
+        }
+        
+        [updatedPicks[index], updatedPicks[targetIndex]] = [updatedPicks[targetIndex], updatedPicks[index]];
+
+        setNewPicks(updatedPicks);
     }
+
 
     useEffect(() => {
         const fetchRankings = async () => {
@@ -67,6 +68,7 @@ const AllianceTools = ({eventCode}: IProps) => {
         fetchRankings();
     }, []);
 
+
     useEffect(() => {
         if (rankings.length > 0) {
             setDisplayRankings(rankings);
@@ -74,18 +76,64 @@ const AllianceTools = ({eventCode}: IProps) => {
     }, [rankings]);
 
     useEffect(() => {
-        setDisplayRankings(rankings.filter((ranking) => !alliances.includes(ranking.teamNumber)));
-    }, [alliances]);
+        setDisplayRankings(rankings.filter((ranking) => !picks.includes(ranking.teamNumber)));
+    }, [picks]);
+
+    useEffect(() => {
+        setPicks(newPicks);
+    }, [newPicks]);
+
+
  return (
-    <Card>
-        <CardContent>
-            <Label>Alliance Pick Name</Label>
-            <Input placeholder="Alliance Pick list Name" required onChange={(e) => {setData({...data, PickListName: e.target.value})}}/>
+
+    <div className="grid grid-cols-2 p-4 gap-2">
+
+        <div className="flex flex-col text-right text-xs p-2">
+            <div className="flex flex-row text-lg font-bold justify-end place-items-end">List Name <ArrowRight /></div>
+            <div>Duplicate names will overwrite existing pick lists.</div>
+        </div>
+
+        <div className="flex flex-col p-2">
+            <input type="text" placeholder="Choose a name for your list." value={listName} onChange={(e) => setListName(e.target.value)} className="w-full p-2 text-sm font-bold bg-neutral-800 rounded-xl text-white" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+            <h1 className="text-lg font-bold">Event Rankings</h1>
+            <p>Select the teams to include in your pick list.</p>
+            {displayRankings.map((ranking, index) => (
+                <div key={index} className="grid grid-cols-[1fr_2fr_5fr_1fr] p-2 gap-2 bg-neutral-800 text-white text-xs rounded-xl justifly-center place-items-center">
+                    <div>{ranking.rank}</div>
+                    <div>{ranking.teamNumber}</div>
+                    <div>{getTeamName(ranking.teamNumber)}</div>
+                    <div><CircleArrowRight onClick={() => addSelection(ranking.teamNumber)} /></div>
+            </div>))}
+        </div>
+
+        <div className="flex flex-col gap-2">
+            <h1 className="text-lg font-bold">Your Picks</h1>
+            <p>Use the arrows to change the order in the list.
+                <br />
+                {newPicks.length > 0 && <span className="text-red-500">{newPicks.join(', ')}</span>}
+            </p>
+            {newPicks.sort().map((pick, index) => (
+                <div key={index} className="grid grid-cols-[1fr_2fr_5fr_1fr] p-2 gap-2 bg-blue-200 text-black text-xs rounded-xl justifly-center place-items-center">
+                    <div>{index + 1}</div>
+                    <div className="text-lg">{pick}</div>
+                    <div>{getTeamName(pick)}</div>
+                    <div>
+                        <CircleArrowUp onClick={() => moveSelection(index, "up")} />
+                        <CircleArrowDown onClick={() => moveSelection(index, "down")} />
+                        <CircleMinus onClick={() => subtractSelection(pick)} />
+                        
+                    </div>
+                </div>))}
             
-            
-            <Button onClick={sendtoDB}>Submit</Button>
-        </CardContent>
-    </Card>
+        </div>
+
+    </div>
+
 
 
  )}
+
+export default AlliancePickForm;
