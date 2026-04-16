cb6b4429e75b0bfd5b177a0f009157ab938d4f49
Caleb H - Student <masterfrederick7410@gmail.com>
2026-04-16 18:22:26 -0400

    FInally fixed the display to show when pressed

--- components/scout/ScoutMenu.tsx
+++ components/scout/ScoutMenu.tsx
@@ -30,7 +30,7 @@ const ScoutMenu = ({setDisplay}: Props) => {
                 <ChartNoAxesCombined className="h-6 w-6" /> 
                 <p className="text-[8px]">Insights</p>
             </button>
-            <button className="flex flex-col justify-center place-items-center text-center bg-blue-900 text-white rounded-lg px-2 py-2 font-bold" onClick={() => setDisplay('main')}>
+            <button className="flex flex-col justify-center place-items-center text-center bg-blue-900 text-white rounded-lg px-2 py-2 font-bold" onClick={() => setDisplay('alliance')}>
                 <Handshake className="h-6 w-6" /> 
                 <p className="text-[8px]">Alliance Tools</p>
             </button>
